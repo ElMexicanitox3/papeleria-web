1d610a92226b0e6c623d4e9f47cc0e73cd57d551
Gerardo Matadama Peralta <gerardo.matadama@gmail.com>
2023-06-21 20:34:32 -0600

    notas de las rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+// El ordne importa
+// supongamos que tenemos una ruta /users/1 y otra /users/new
+// si ponemos la ruta /users/new primero, al entrar a /users/1 nos va a mandar a /users/new
+// porque new es un parametro
+Route::get('/users/new', function () {
+    return 'Crear nuevo usuario';
+});
+
+Route::get('/users/{id}', function ($id) {
+    return "Mostrando detalle del usuario: {$id}";
+});
+
+// podemos poner un parametro opcional
+Route::get('/saludo/{name}/{apellidos?}', function ($name, $apellidos = null) {
+    // Validamos si tiene solo un nombre o nombre y apellidos
+    if ($apellidos) {
+        return "Hola {$name} {$apellidos}";
+    }
+    return "Hola {$name}";
+});
