e4292d60416ad0acf6c52ec3752cd4a169803226
Gerardo Matadama Peralta <gerardo.matadama@gmail.com>
2023-06-30 22:56:53 -0600

    video 15

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -42,4 +44,32 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+
+    //  Atributo personalizado
+    protected function name(): Attribute {
+        return new Attribute(
+
+            // Accesor de atributo #1
+            // get: function ($value) {
+            //     return ucwords($value);
+            // }, 
+
+            // Accesor de atributo #2 manera alternativa
+            get: fn($value) => ucwords($value),
+
+            // Mutador de atributo
+            // set: function ($value) {
+            //     return strtolower($value);
+            // },
+
+            // Mutador de atributo manera alternativa
+            set: fn($value) => strtolower($value)
+        );
+    }
 }
